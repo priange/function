cb90bc8643b64a62892360900652e8fa66fcc00c
priange <ityakup@gmail.com>
2023-04-12 20:41:25 +0300

    first commit

---
+++ JavaScript/String.js
@@ -0,0 +1,58 @@
+//1. Write a function that takes a string as input and returns the length of the string.
+function word(str){
+return str.length;
+}
+const string=`This is my son.`
+let x=word(string)
+
+console.log(x)
+
+function real(Strings){
+return Strings.length;
+}
+const really='Hard work pays'
+let a=real(really);
+console.log(a);
+
+
+
+// 2.Write a function that takes a string as input and returns the string reversed.
+
+function river(String){
+    return String.split().reverse().join();
+}
+const will='It will need sacrifice'
+let i=river(will)
+console.log(i)
+
+
+
+function laked(str){
+    return str.split().reverse().join();
+}
+const text='This is worth it'
+let z=laked(text);
+console.log(z)
+
+
+
+//3.Write a function that takes two strings as input and returns the concatenation of the two strings.
+function conned(String){
+    return c.concat(d)
+}
+let c="God's"
+let d="wisdom"
+const all=c + " " +d
+let y=conned(all)
+console.log(y)
+
+
+// 4.Write a function that takes a string as input and returns
+// a new string with all the words capitalized.
+function caps(string){
+
+return string.toUpperCase()
+}
+let q="Campus"
+const give=caps(q)
+console.log(give)
---
+++ JavaScript/Strings.js
@@ -0,0 +1,16 @@
+// Given the String below,perform inbuilt functions in JavaScript to 
+// solve it appropriately.
+// let x="There is a dog outside"
+const message='There is a dog outside'
+console.log(message.length)
+let stmt='There is a dog outside'
+console.log(stmt.includes('there'))
+console.log(stmt.toUpperCase())
+console.log(message.includes('is a'))
+//creating a message on two lines.
+let tell="There is my son so grown up,\n intelligent and God fearing."
+console.log(tell)
+//starts with a particular letter
+console.log(message.startsWith('There'))
+console.log(message.endsWith('d'))
+console.log(indexOf('is'))
